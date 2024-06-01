29a5e84f3a4ee6c33314379d622bf9e542ef61d6
jessadaSrm <jessada.srm@gmail.com>
2024-06-01 23:04:59 +0700

    edit comments

--- main.js
+++ main.js
@@ -3,8 +3,8 @@ import { sleep, check } from "k6";
 
 export let options = {
 	stages: [
-		{ duration: "1m", target: 1500 }, // ramp-up to 10 users over 30 seconds
-		{ duration: "5m", target: 1500 }, // stay at 10 users for 1 minute
+		{ duration: "1m", target: 1500 }, // ramp-up to 1500 users over 1 minute
+		{ duration: "5m", target: 1500 }, // stay at 1500 users for 5 minute
 		{ duration: "30s", target: 0 }, // ramp-down to 0 users over 30 seconds
 	],
 };
